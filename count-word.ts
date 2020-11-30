312b9f07634802d8648981f0694cb856a578a7a1
AngelJGR <ajguerreror@gmail.com>
2020-11-30 18:08:04 -0400

    Crea metodo contador de palabras

---
+++ count-word.ts
@@ -0,0 +1,23 @@
+let words = "Y así como suele decirse el gato al rato, el rato a la cuerda, la cuerda al palo; daba el arriero a Sancho, Sancho a la moza, la moza a él, el ventero a la moza."
+
+function normalizer (word: string) {
+    return word.toLowerCase().normalize('NFD')
+        .replace(/([aeio])\u0301|(u)[\u0301\u0308]|[.!,;]/gi,"$1$2")
+        .normalize()
+}
+
+function countWords(text: string) {
+    let dictionary: {[key: string]: number} = {}
+    let separateWords: Array<string> = text.split(" ")
+
+    for (let word of separateWords) {
+        if (normalizer(word) in dictionary) {
+            ++dictionary[normalizer(word)]
+        } else {
+            dictionary[normalizer(word)] = 1
+        }
+    }
+    console.log(dictionary)
+}
+
+countWords(words);
